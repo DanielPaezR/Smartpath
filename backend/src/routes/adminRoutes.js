ab063470ff2f631f84030ee67e2bfdb1b212035c
Daniel Paez <daniel.paezr@unac.edu.co>
2026-02-18 18:13:43 -0500

    add: agregué opcion de registrar reposiciones

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import adminController from '../controllers/adminController';
+import adminController from '../controllers/adminController.js';
 import { authenticateToken, requireRole } from '../middleware/auth.js';
 import { advancedAdminController } from '../controllers/advancedAdminController.js';
 
