c5e1b012882dcb7e30c2166524a284708901f0b4
Daniel Paez <daniel.paezr@unac.edu.co>
2026-02-18 15:25:01 -0500

    add: agregué opcion de registrar reposiciones

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -28,59 +28,59 @@ export default defineConfig({
         background_color: '#FFFFFF',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/~daniel.paez/smartpath/',
-        start_url: '/~daniel.paez/smartpath/#/login', // 👈 CORREGIDO: apunta al login
+        scope: './',
+        start_url: './#/login', // 👈 CORREGIDO: apunta al login
         icons: [
           {
-            src: '/~daniel.paez/smartpath/icon-72x72.png',
+            src: './icon-72x72.png',
             sizes: '72x72',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-96x96.png',
+            src: './icon-96x96.png',
             sizes: '96x96',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-128x128.png',
+            src: './icon-128x128.png',
             sizes: '128x128',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-144x144.png',
+            src: './icon-144x144.png',
             sizes: '144x144',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-152x152.png',
+            src: './icon-152x152.png',
             sizes: '152x152',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-192x192.png',
+            src: './icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-384x384.png',
+            src: './icon-384x384.png',
             sizes: '384x384',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/icon-512x512.png',
+            src: './icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/~daniel.paez/smartpath/maskable-icon-512x512.png',
+            src: './maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
